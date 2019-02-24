0bfd7b443bcf755ce911638fe2fddf0bb9aefc69
mkhan <riponk9519@gamil.com>
2019-02-23 22:50:14 -0600

    Added new code in Article.js

---
+++ models/Article.js
@@ -0,0 +1,38 @@
+/////////////////////////////////////////////// /* Imports */ //////////////////////////////////////////////////////////
+var mongoose = require("mongoose");
+
+// Save a reference to the Schema constructor
+var Schema = mongoose.Schema;
+
+// Using the Schema constructor, create a new UserSchema object
+// This is similar to a Sequelize model
+// Create article schema
+var ArticleSchema = new Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  link: {
+    type: String,
+    required: true
+  },
+  condensed: {
+    type: String,
+    required: true
+    
+  },
+  saved: {
+    type: Boolean,
+    default: false
+  },
+  note: [{
+    type: Schema.Types.ObjectId,
+    ref: "Note"
+  }]
+});
+
+// Create the Article model with the ArticleSchema
+var Article = mongoose.model("Article", ArticleSchema);
+
+// Export the model
+module.exports = Article;
