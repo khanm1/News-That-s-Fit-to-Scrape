a0249faa3a8bae8fbd8e10e03c1474ff685a74cc
mkhan <riponk9519@gamil.com>
2019-03-01 18:17:00 -0600

    Edited codes in Articles.js

--- models/Article.js
+++ models/Article.js
@@ -3,8 +3,6 @@ var mongoose = require("mongoose");
 // Save a reference to the Schema constructor
 var Schema = mongoose.Schema;
 
-// Using the Schema constructor, create a new UserSchema object
-// This is similar to a Sequelize model
 // Create article schema
 var ArticleSchema = new Schema({
   title: {
